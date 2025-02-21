81093f8ac91f5652730978f31c26b8417e84db9a
Hlieb Medvediev <gmedvede226@gmail.com>
2025-02-21 20:15:55 +0200

    some changes, added README.md file

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,8 +1,9 @@
 import asyncio
 import time
 from collections import defaultdict
-from aiogram.dispatcher.event.bases import CancelHandler
 from aiogram import BaseMiddleware
+from aiogram.types import Update, Message, CallbackQuery
+from aiogram.dispatcher.event.bases import CancelHandler
 
 user_messages = defaultdict(list)
 spam_users = {}
@@ -14,16 +15,27 @@ class ThrottlingMiddleware(BaseMiddleware):
         self.rate_limit = rate_limit
         self.user_timers = {}
 
-    async def on_process_message(self, message, data):
-        user_id = message.from_user.id
+    async def __call__(self, handler, event: Update, data):
+        message: Message = getattr(event, "message", None)
+        callback: CallbackQuery = getattr(event, "callback_query", None)
+
+        user = message.from_user if message else callback.from_user if callback else None
+        if not user:
+            return await handler(event, data)
+
+        user_id = user.id
+
         if user_id in self.user_timers:
             raise CancelHandler()
+
         self.user_timers[user_id] = True
         await asyncio.sleep(self.rate_limit)
         del self.user_timers[user_id]
 
+        return await handler(event, data)
 
-async def limit_messages(message):
+
+async def limit_messages(message: Message):
     user_id = message.from_user.id
     current_time = time.time()
 
