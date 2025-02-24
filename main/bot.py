a05a2ea5180cf41c0acc53cdf42d08ac4d9aae5b
Hlieb Medvediev <gmedvede226@gmail.com>
2025-02-24 18:16:12 +0200

    removed spam protection because of incorrect work

--- main/bot.py
+++ main/bot.py
@@ -3,14 +3,12 @@ from aiogram import Bot, Dispatcher
 from databases.user_database import user_database
 from handlers.user_handlers import user_router
 from common.token import TOKEN
-from middlewares.throttling import ThrottlingMiddleware
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 db = user_database()
 
 dp.include_router(user_router)
-dp.update.middleware(ThrottlingMiddleware())
 
 
 async def main():
